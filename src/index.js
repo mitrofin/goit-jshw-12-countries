72d6bbfc9ac2cc1db6e9470a2594848bbe69ba4d
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-04-15 19:12:16 +0300

    create templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css';
+/* import fetch from './js/fetchCountries'; */
+
+ fetch(`https://restcountries.eu/rest/v2/name/swit`)
+        .then(response => {
+            /* if (response.ok)  return response.json(); */
+             if (response.status === 404) {
+                return response.json();}
+            return response.json();
+            
+        }).then(data => { console.log(data); });
+    
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+function fetchCountries(searchQuery) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+        .then(response => {
+            /* if (response.ok)  return response.json(); */
+             if (response.status === 404) {
+                return response.json();}
+            return response.json();
+            
+        }).then(data => console.log(data));
+    
+}
+
+export default {fetchCountries};
