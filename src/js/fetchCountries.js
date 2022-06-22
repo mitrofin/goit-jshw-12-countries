8eab5a42b64737946463b14fdc1bb3df536efdb3
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-06-22 22:28:52 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,9 @@
 function fetchCountries(searchQuery) {
+<<<<<<< Updated upstream
  return fetch(`https://restcountries.eu/rest/name/${searchQuery}`).then(
+=======
+ return fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(
+>>>>>>> Stashed changes
   response => {
    /* if (response.status === 404) {
     console.log('УПС');
